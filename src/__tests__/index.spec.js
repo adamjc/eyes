a0ab4e68f0d9abcef3f0f9b27d966a29740e419e
adamjc <adamjc86@gmail.com>
2019-08-31 22:46:30 +0100

    Add barebones test

---
+++ src/__tests__/index.spec.js
@@ -0,0 +1,7 @@
+
+
+describe('the game', () => {
+  it('can start a new game', () => {
+    // start a game, try and start another game with the same id, expect the message to be 'game already exists' etc.
+  })
+})
