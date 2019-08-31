96710e18824c9f6a524ab7f5ad5342bce0a96705
adamjc <adamjc86@gmail.com>
2019-08-31 22:46:06 +0100

    Parse incoming payload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
+const querystring = require('querystring')
+
 exports.handler = (event, context, callback) => {
-  callback(null, {
-    "statusCode": 200,
-    "body": JSON.stringify("Hello, World!")
-  })
+  if (!event.body || typeof event.body !== 'string') {
+    callback(null, {
+      "statusCode": 400,
+      "body": JSON.stringify(`Incorrect payload: ${event.body}`)
+    })
+  }
+
+  try {
+    const payload = querystring.parse(event.body)
+
+    console.log(payload.command)
+    console.log(payload.user_name)
+    console.log(payload.text)
+
+    // handle `/eyes start`
+    callback(null, {
+      "statusCode": 200,
+      "body": JSON.stringify("Hello, World!")
+    })
+
+    // handle `/eyes :celebrity`
+  } catch (err) {
+    console.log(`err: ${JSON.stringify(err, null, 2)}`)
+  }
 }
